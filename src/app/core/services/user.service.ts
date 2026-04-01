20adc39bb915719110e578ff3556c6f3c8063d54
Antonio Ortiz Avila <antonio.ortiz.avila@gmail.com>
2026-04-01 11:51:41 -0600

    Chore: Refactor model imports to use barrel files

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,15 +2,15 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ApiResponse } from '../models/api-response.model';
-import { Page } from '../models/pagination.model';
+import { ApiResponse } from '../models';
+import { Page } from '../models';
 import {
   UserResponse,
   CreateUserRequest,
   UpdateUserRequest,
   UpdateProfileRequest,
   AssignRolesRequest,
-} from '../models/user.model';
+} from '../models';
 
 export interface UserFilterRequest {
   page: number;
