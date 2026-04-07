8d0186c0cf75a3642b38f972a65385829f7774b6
Antonio Ortiz Avila <antonio.ortiz.avila@gmail.com>
2026-04-07 12:10:27 -0600

    feat(http): Migrate to provideHttpClient and functional interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,28 @@
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ClipboardModule } from 'ngx-clipboard';
 import { TranslateModule } from '@ngx-translate/core';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthService } from './modules/auth/services/auth.service';
-import { environment } from 'src/environments/environment';
+import { AuthService } from './modules/auth';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { ToastContainerComponent } from './shared/components/toast-container/toast-container.component';
 import { GlobalAlertContainerComponent } from './shared/components/global-alert-container/global-alert-container.component';
-import { AuthInterceptorService } from './core/interceptors/auth-interceptor.service';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorInterceptorService } from './core/interceptors/error-interceptor.service';
-// #fake-start#
-import { FakeAPIService } from './_fake/fake-api.service';
-// #fake-end#
+import {authInterceptor} from "./core/interceptors/auth.interceptor";
+import { lastValueFrom } from 'rxjs';
 
 function appInitializer(authService: AuthService) {
   return () => {
-    return new Promise((resolve) => {
-      //@ts-ignore
-      authService.getUserByToken().subscribe().add(resolve);
-    });
+    return lastValueFrom(authService.getUserByToken())
+      .catch((err) => {
+        console.warn('Error en la carga inicial del usuario:', err);
+        return null;
+      });
   };
 }
 
@@ -35,16 +32,7 @@ function appInitializer(authService: AuthService) {
     BrowserModule,
     BrowserAnimationsModule,
     TranslateModule.forRoot(),
-    HttpClientModule,
     ClipboardModule,
-    // #fake-start#
-    environment.isMockEnabled
-      ? HttpClientInMemoryWebApiModule.forRoot(FakeAPIService, {
-        passThruUnknownUrl: true,
-        dataEncapsulation: false,
-      })
-      : [],
-    // #fake-end#
     AppRoutingModule,
     InlineSVGModule.forRoot(),
     NgbModule,
@@ -59,11 +47,10 @@ function appInitializer(authService: AuthService) {
       multi: true,
       deps: [AuthService],
     },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+      withInterceptorsFromDi()
+    ),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptorService,
